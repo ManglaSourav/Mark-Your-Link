22c3d39e50fbe9494452412dcc6b599b7e900ced
Sourav Mangla <mangla.sourav96@gmail.com>
2019-04-10 02:26:09 +0530

    make it better

--- server/app.js
+++ server/app.js
@@ -36,7 +36,7 @@ app.use("/users", usersRoutes);
 // Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('../client/build'));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
